08c341c99e5516a96ab7ff229c8b0c803bf85989
oponomarenko <oponomarenko@oponomarenko-a2442.local>
2025-11-18 19:15:31 +0200

    revert test file

--- apps/web/src/app/app.spec.ts
+++ apps/web/src/app/app.spec.ts
@@ -7,13 +7,4 @@ describe('App', () => {
       imports: [App],
     }).compileComponents();
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome nx-project-web'
-    );
-  });
 });
